8abc87d6ba18806026a8b4635fd29c7a1ea09d09
Chris Repanich <chrisrepanich@gmail.com>
2020-06-01 11:36:30 -0700

    add scoreboard to end round scoring view

--- src/views/EndRound/ScoringView.js
+++ src/views/EndRound/ScoringView.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 import ScoringAnswerList from './AnswerList/ScoringAnswerList';
 import ScoringHeaderBar from './HeaderBar/ScoringHeaderBar';
+import ScoreboardView from '../Scoreboard/ScoreboardView';
+
 import './ScoringView.css';
 
 const ScoringView = () => {
   return (
     <div id="scoring-view-container">
-      <ScoringHeaderBar />
-      <ScoringAnswerList />
+      <ScoreboardView />
+      <div id="score-container">
+        <ScoringHeaderBar />
+        <ScoringAnswerList />
+      </div>
     </div>
   );
 };
